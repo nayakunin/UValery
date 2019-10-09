0d326cce3a56c7b86f1669278f2ddd7cbfcc4551
Yakunin Nikita <nayakunin@outlook.com>
2019-10-09 16:56:48 +0300

    added fancy multi-select filters

--- src/index.js
+++ src/index.js
@@ -1,8 +1,99 @@
 import "./style.css";
 
-const contactBtn = document.querySelector(".contact__btn");
+class Select {
+    constructor(DOMElement) {
+        this.DOMElement = DOMElement;
+        this.placeholder = DOMElement.querySelector('.select__text_placeholder');
+        this.resultContainer = DOMElement.querySelector('.select__text_result');
+        this.resultTextContainer = DOMElement.querySelector('.select__result');
+        this.resultCloseBtn = DOMElement.querySelector('.select__result-close');
+        this.result = [];
+        this.optionsContainer = DOMElement.querySelector('.select__option-container');
+        this.options = DOMElement.querySelectorAll('.select__checkbox');
+        this.dropdownBtn = DOMElement.querySelector('.select__dropdown-btn');
 
-const inputDropdown = document.querySelectorAll(".filter__item_dropdown");
+        this.options.forEach(function(item) {
+            item.addEventListener('click', function(elem) {
+                // console.log(item.parentNode.querySelector('select__item-text'));
+                this.togglePick(item.parentNode.querySelector('.select__item-text').textContent);
+            }.bind(this));
+        }.bind(this));
+
+        this.DOMElement.addEventListener('click', function(event) {
+            if (this.optionsContainer.classList.contains('select__option-container_hidden')) {
+                this.showDropdown();
+            } else {
+                if (event.target === this.dropdownBtn) {
+                    this.hideDropdown();
+                }
+            }
+        }.bind(this));
+
+        this.resultCloseBtn.addEventListener('click', this.uncheckALL.bind(this));
+    }
+    updateResult() {
+        if (this.result.length > 1) {
+            this.showResult(`Выбрано ${this.result.length}`);
+        } else if (this.result.length === 1) {
+            this.showResult(this.result[0]);
+        } else {
+            this.hideResult();
+        }
+    }
+    togglePick(str) {
+        if (!this.result.includes(str)){
+            this.result.push(str);
+        } else {
+            let index = this.result.indexOf(str);
+            if (index > -1) {
+                this.result.splice(index, 1);
+            }
+        }
+        this.updateResult();
+    }
+    uncheckALL() {
+        this.options.forEach(function(item) {
+            item.checked = false;
+        });
+        this.result = [];
+        this.updateResult();
+    }
+    toggleDropdownBtn() {
+        this.dropdownBtn.classList.toggle('select__dropdown-btn_active');
+    }
+    hidePlaceholder() {
+        this.placeholder.classList.add('select__text_placeholder-hidden');
+    }
+    showPlaceholder() {
+        this.placeholder.classList.remove('select__text_placeholder-hidden');
+    }
+    hideResult() {
+        this.resultContainer.classList.add('select__text_result-hidden');
+    }
+    showResult(str) {
+        this.resultContainer.classList.remove('select__text_result-hidden');
+        this.resultTextContainer.textContent = str;
+    }
+    showDropdown() {
+        this.optionsContainer.classList.remove('select__option-container_hidden');
+        this.hidePlaceholder();
+        this.toggleDropdownBtn();
+    }
+    hideDropdown() {
+        this.optionsContainer.classList.add('select__option-container_hidden');
+        if (this.result.length === 0) {
+            this.showPlaceholder();
+        }
+        this.toggleDropdownBtn();
+    }
+}
+
+const citySelect = new Select(document.querySelector('.select_city'));
+const skillsSelect = new Select(document.querySelector('.select_skills'));
+const scheduleSelect = new Select(document.querySelector('.select_schedule'));
+const experienceSelect = new Select(document.querySelector('.select_experience'));
+
+const contactBtn = document.querySelector(".contact__btn");
 
 if (contactBtn) {
     contactBtn.addEventListener("click", function(event) {
@@ -14,42 +105,3 @@ if (contactBtn) {
     })
 }
 
-if (inputDropdown) {
-    inputDropdown.forEach(function(item) {
-        const pick = [];
-        const input = item.querySelector(".input__text");
-        const currentInput = item;
-        const currentDropdown = item.querySelector(".input__dropdown");
-        document.querySelector(".body").addEventListener("click", function(event) {
-            if (!(event.target == currentInput || event.target == currentDropdown)) {
-                currentDropdown.classList.add("dropdown_hidden");
-            }
-        })
-        item.addEventListener("input", function(event) {
-            currentDropdown.classList.remove("dropdown_hidden");
-            if (input.value === "") {
-                currentDropdown.classList.add("dropdown_hidden");
-            }
-        })
-        item.querySelectorAll(".dropdown__checkbox").forEach(function(dropdownItem) {
-            dropdownItem.addEventListener("click", function (event) {
-                let value = event.target.parentNode.querySelector(".dropdown__item-text").textContent;
-                if (!pick.includes(value)) {
-                    pick.push(value);
-                } else {
-                    let index = pick.indexOf(value);
-                    if (index > -1) {
-                        pick.splice(index, 1);
-                    }
-                }
-                if (pick.length > 1) {
-                    input.value = `Выбрано ${pick.length}`;
-                } else if (pick.length == 1) {
-                    input.value = pick[0];
-                } else {
-                    input.value = " ";
-                }
-            })
-        })
-    })
-}
